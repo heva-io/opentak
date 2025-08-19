4c433be326f7cfdea47477c426ad75f6f8b48d84
marasendra <marasendra@hevaweb.com>
2025-08-19 18:16:47 +0200

    fix: add # type: ignore[import-not-found] in gen_ref_pages.py to fix mypy error

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -7,7 +7,7 @@ https://mkdocstrings.github.io/recipes/#bind-pages-to-sections-themselves
 from pathlib import Path
 
 
-import mkdocs_gen_files
+import mkdocs_gen_files  # type: ignore[import-not-found]
 
 nav = mkdocs_gen_files.Nav()
 
